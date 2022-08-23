f5335988b170342520e9d865620a1cdf0bc9510a
Tim Takita <takitatd@gmail.com>
2022-08-24 04:30:54 +1000

    deletion works

--- src/bl_menu/bl_delete.rs
+++ src/bl_menu/bl_delete.rs
@@ -14,39 +14,40 @@ pub fn use_bl(
 ) -> Result<(), std::io::Error> {
     let mut stdout = io::stdout();
     let mut stdin = io::stdin();
-    let mut count = 0;
-    let mut entries = OpenOptions::new()
-        .read(true)
-        .open(bl_fs::bl_file())
-        .unwrap();
-    let mut entries_str = String::new();
-    entries.read_to_string(&mut entries_str);
-    let mut entries = serde_json::from_str::<Vec<bl_types::Entry>>(
-        magic
-            .decrypt_base64_to_string(entries_str)
-            .unwrap()
-            .as_str(),
-    )
-    .unwrap();
-    for entry in &mut entries {
-        count += 1;
-        entry.chrono = count;
-        writeln!(
-            stdout,
-            "{}. {}Platform: {}, Username: {}{}",
-            entry.chrono,
-            SetForegroundColor(Color::Rgb {
-                r: 219,
-                g: 87,
-                b: 120
-            }),
-            entry.platform,
-            entry.username,
-            SetForegroundColor(Color::White)
-        )?;
-    }
-    writeln!(stdout, "Enter \"qq\" to quit.")?;
     loop {
+        let mut count = 0;
+
+        let mut entries = OpenOptions::new()
+            .read(true)
+            .open(bl_fs::bl_file())
+            .unwrap();
+        let mut entries_str = String::new();
+        entries.read_to_string(&mut entries_str);
+        let mut entries = serde_json::from_str::<Vec<bl_types::Entry>>(
+            magic
+                .decrypt_base64_to_string(entries_str)
+                .unwrap()
+                .as_str(),
+        )
+        .unwrap();
+        for entry in &mut entries {
+            count += 1;
+            entry.chrono = count;
+            writeln!(
+                stdout,
+                "{}. {}Platform: {}, Username: {}{}",
+                entry.chrono,
+                SetForegroundColor(Color::Rgb {
+                    r: 219,
+                    g: 87,
+                    b: 120
+                }),
+                entry.platform,
+                entry.username,
+                SetForegroundColor(Color::White)
+            )?;
+        }
+        writeln!(stdout, "Enter \"qq\" to quit.")?;
         write!(stdout, "Search: ")?;
         let mut buf = String::new();
         stdout.flush()?;
@@ -100,27 +101,6 @@ pub fn del(magic: MagicCrypt256, entry_index: u32, entries: &Vec<bl_types::Entry
     let mut stdout = io::stdout();
     let mut entes = entries.to_vec();
     entes.remove(entry_index as usize);
-    let mut count = 0;
-    // writeln!(stdout, "{:?}", entes);
-
-    for entry in &mut entes {
-        count += 1;
-
-        writeln!(
-            stdout,
-            "{}. {}Platform: {}, Username: {}{}",
-            entry.chrono,
-            SetForegroundColor(Color::Rgb {
-                r: 219,
-                g: 87,
-                b: 120
-            }),
-            entry.platform,
-            entry.username,
-            SetForegroundColor(Color::White)
-        );
-    }
-
     let mut entries_file = OpenOptions::new()
         .read(true)
         .write(true)
@@ -135,7 +115,7 @@ pub fn del(magic: MagicCrypt256, entry_index: u32, entries: &Vec<bl_types::Entry
                 .as_bytes(),
         )
         .unwrap();
-    std::process::exit(0);
+    // std::process::exit(0);
 }
 
 pub fn delete(magic: MagicCrypt256) -> Result<(), std::io::Error> {
